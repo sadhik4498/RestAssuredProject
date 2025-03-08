95388608d37c0243ad4c38c9a6bdd67eaba78604
2327327 <2327327@LTIN399820.cts.com>
2025-03-09 00:57:21 +0530

    Created API project

---
+++ src/test/java/api/endpoints/Routes.java
@@ -0,0 +1,20 @@
+package api.endpoints;
+
+public class Routes {
+	
+//	Post : petstore.swagger.io/v2/user
+//	Get: petstore.swagger.io/v2/User/{Username}
+//	Put: petstore.swagger.io/v2/User/{Username}
+//	Delete: petstore.swagger.io/v2/User/{Username
+	
+	public static String base_url ="petstore.swagger.io/v2";
+	
+	//end pints for Store API
+	public static String post_url = base_url+"User";
+	public static String get_url = base_url+"User/{Username}";
+	public static String update_url = base_url+"User/{Username}";
+	public static String delete_url = base_url+"User/{Username}";
+	
+	
+
+}
